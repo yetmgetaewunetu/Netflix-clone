489184e67c0716f024eb34e702e61d2b61a89f60
Yetmgeta Ewunetu <yetmgetae@gmail.com>
2024-10-21 11:37:08 +0300

    updating the fetch method

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,7 +1,7 @@
-const API_KEY = "ada6e4c7b4f1f5b110a217a26add93a2";
+const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
-  fetchtrending: `/trending/movie/day?language=en-US&api_key=ada6e4c7b4f1f5b110a217a26add93a2`,
-<<<<<<< HEAD
+  fetchtrending: `/trending/movie/day?language=en-US&api_key=${API_KEY}`,
+
   fetchNetflixOriginals: `/discover/tv?with_networks=213&api_key=${API_KEY}`,
   fetchTopRatedMovies: `/movie/top_rated?language=en-US&api_key=${API_KEY}`,
   fetchActionMovies: `/discover/movie?with_genres=28&api_key=${API_KEY}`,
@@ -10,16 +10,6 @@ const requests = {
   fetchRomanticMovies: `/discover/movie?with_genres=10749&api_key=${API_KEY}`,
   fetchDocumentaries: `/discover/movie?with_genres=99&api_key=${API_KEY}`,
   fetchTvShow: `tv/popular?language=en-US&api_key=${API_KEY}&page=1`,
-=======
-  fetchNetflixOriginals: `/discover/tv?with_networks=213&api_key=ada6e4c7b4f1f5b110a217a26add93a2`,
-  fetchTopRatedMovies: `/movie/top_rated?language=en-US&api_key=ada6e4c7b4f1f5b110a217a26add93a2`,
-  fetchActionMovies: `/discover/movie?with_genres=28&api_key=ada6e4c7b4f1f5b110a217a26add93a2`,
-  fetchComedyMovies: `/discover/movie?with_genres=35&api_key=ada6e4c7b4f1f5b110a217a26add93a2`,
-  fetchHorrorMovies: `/discover/movie?with_genres=27&api_key=ada6e4c7b4f1f5b110a217a26add93a2`,
-  fetchRomanticMovies: `/discover/movie?with_genres=10749&api_key=ada6e4c7b4f1f5b110a217a26add93a2`,
-  fetchDocumentaries: `/discover/movie?with_genres=99&api_key=ada6e4c7b4f1f5b110a217a26add93a2`,
-  fetchTvShow: `tv/popular?language=en-US&api_key=ada6e4c7b4f1f5b110a217a26add93a2&page=1`,
->>>>>>> 652864ab9a77ad15e2b766e31ee103b3f5999a5d
 };
 
 export default requests;
